3708e9a3a48b26fb4980aa730b608db0ba66be06
Quba1 <jak.lewand@gmail.com>
2021-06-25 17:56:54 +0200

    into to building from source

--- build.rs
+++ build.rs
@@ -1,37 +1,25 @@
-use std::{collections::HashSet, fs::File, path::PathBuf, str::FromStr, sync::{Arc, RwLock}};
+use std::{
+    collections::HashSet,
+    fs::File,
+    path::PathBuf,
+    sync::{Arc, RwLock},
+};
 
 use bindgen::callbacks::{MacroParsingBehavior, ParseCallbacks};
 use flate2::read::GzDecoder;
 use tar::Archive;
 
-/*
-default source build memfs+static
-
-if cfg(build-source)
-    build_source()
-else nothing
-    search with pkg-config
-    if failed
-        build_source()
-
-build_source() {
-    if cfg(shared)
-        BUILD_SHARED_LIBS=ON
-    else
-        BUILD_SHARED_LIBS=OFF
-
-    if config(no_memfs)
-        ENABLE_MEMFS=OFF
-    else
-        ENABLE_MEMFS=ON
-}
- */
-
 // build script heavily inspired by proj-sys crate
 // some parts of code from rust-bindgen
 
 const MINIMUM_ECCODES_VERSION: &str = "2.20.0"; // currently the latest in apt-get
-const PROBLEMATIC_MACROS: [&str; 5] = ["FP_NAN", "FP_INFINITE", "FP_ZERO", "FP_SUBNORMAL", "FP_NORMAL"];
+const PROBLEMATIC_MACROS: [&str; 5] = [
+    "FP_NAN",
+    "FP_INFINITE",
+    "FP_ZERO",
+    "FP_SUBNORMAL",
+    "FP_NORMAL",
+];
 
 #[derive(Debug)]
 struct MacroCallback {
@@ -79,7 +67,7 @@ fn main() {
     //bindgen magic to avoid duplicate math.h type definitions
     let macros = Arc::new(RwLock::new(HashSet::new()));
 
-    let tests = cfg!(feature="tests");
+    let tests = cfg!(feature = "tests");
     eprintln!("{:?}", include_path);
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}", include_path.to_string_lossy()))
@@ -101,14 +89,63 @@ fn main() {
         .expect("Failed to write bindings to file");
 }
 
-fn get_include_from_source() -> PathBuf{
+fn get_include_from_source() -> PathBuf {
     eprintln!("Building ecCodes from source so using specified features");
 
+    //unpack archive
     let path = "eccodes-src/eccodes-2.22.1-Source.tar.gz";
     let tar_gz = File::open(path).expect("Failed to open ecCodes source archive");
     let tar = GzDecoder::new(tar_gz);
     let mut archive = Archive::new(tar);
-    archive.unpack("eccodes-src").expect("Failed to unpack ecCodes source archive");
+    archive
+        .unpack("eccodes-src")
+        .expect("Failed to unpack ecCodes source archive");
 
-    PathBuf::from_str("foo.h").unwrap()
+    //build source with cmake
+    let mut cmake_config = cmake::Config::new("eccodes-src/eccodes-2.22.1-Source");
+
+    //default configuration
+    cmake_config.define("CMAKE_C_FLAGS", "-O3");
+    cmake_config.define("ENABLE_NETCDF", "OFF");
+    cmake_config.define("ENABLE_FORTRAN", "OFF");
+    cmake_config.define("ENABLE_JPG", "OFF");
+    cmake_config.define("ENABLE_PNG", "OFF");
+    cmake_config.define("ENABLE_MEMFS", "ON");
+    cmake_config.define("BUILD_SHARED_LIBS", "OFF");
+
+    if cfg!(feature = "shared") {
+        cmake_config.define("BUILD_SHARED_LIBS", "ON");
+    }
+
+    if cfg!(feature = "no_memfs") {
+        cmake_config.define("ENABLE_MEMFS", "OFF");
+    }
+
+    if cfg!(feature = "netcdf") {
+        cmake_config.define("ENABLE_NETCDF", "ON");
+    }
+
+    if cfg!(feature = "jpeg") {
+        cmake_config.define("ENABLE_JPG", "ON");
+    }
+    if cfg!(feature = "png") {
+        cmake_config.define("ENABLE_PNG", "ON");
+    }
+
+    //path to built libeccodes
+    let eccodes = cmake_config.build();
+
+    //link the library
+    if cfg!(feature = "shared") {
+        println!("cargo:rustc-link-lib=eccodes");
+    } else {
+        println!("cargo:rustc-link-lib=static=eccodes");
+    }
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        eccodes.join("lib").display()
+    );
+
+    eccodes.join("include")
 }
